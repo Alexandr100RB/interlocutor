e0d8fedafc96afe1033fc0a8331651b9251a1fae
aleksandrkosanin <kosyanin@yandex.ru>
2023-04-23 17:44:53 +0300

    added try-except

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ openai.api_key = openai_api_key
 bot = Bot(token)
 dp = Dispatcher(bot)
 
+# Здесь сохраняем историю сообщений
 messages = []
 
 
@@ -18,6 +19,7 @@ async def start_bot(_):
     print('Бот запущен')
 
 
+# Приветствие при запуске бота
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await bot.send_message(message.from_user.id,
@@ -26,21 +28,23 @@ async def process_start_command(message: types.Message):
 
 @dp.message_handler()
 async def send(message: types.Message):
-    messages.append({"role": "user", "content": message.text})
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    response = completion.choices[0].message.content
-    messages.append({"role": "assistant", "content": response})
+    try:
+        messages.append({"role": "user", "content": message.text})
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        response = completion.choices[0].message.content
+        messages.append({"role": "assistant", "content": response})
 
-    await message.answer(completion.choices[0].message.content)
+        await message.answer(completion.choices[0].message.content)
+    except Exception:
+        await bot.send_message(message.from_user.id, "что-то пошло не так, попробуй ещё")
 
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_bot)
